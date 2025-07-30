f172eaf1d05e8606a93fc0ba1432174fb5d15be8
MAHI-VASHISTHA <mahivashistha65@gmail.com>
2025-07-30 12:35:42 +0530

    Add files via upload

---
+++ 05 Conditional Statements/IfElseIfElse.java
@@ -0,0 +1,31 @@
+package Statements;
+
+public class IfElseIfElse {
+
+	public static void main(String[] args) {
+
+		int a = 1;
+		int b = 4;
+
+		if (a++ == b) {
+			System.out.println(a++);
+		}
+		else if (a++ == b) {
+			System.out.println(a++);
+		}
+		else if (a++ == b) {
+			System.out.println(a++);
+		}
+		else if (a++ == b) {
+			System.out.println(a++);
+		}
+		else if (a++ == b) {
+			System.out.println(a++);
+		}
+		else {
+		System.out.println(a);
+		
+	}
+}
+
+}
