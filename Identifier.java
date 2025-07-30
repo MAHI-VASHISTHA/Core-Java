f61068bb98dbde4413f17de36b1d6e3b00449a8b
MAHI-VASHISTHA <mahivashistha65@gmail.com>
2025-07-30 11:30:06 +0530

    Add files via upload

---
+++ Identifier.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Identifier {
+public static void main(String [] args) {
+	
+	int a=5;
+	float fl;
+	double $g;
+	int _none;
+	
+//	String void; error-->void is the keyword which does not used at the place of variable.
+  
+	
+	
+}
+}
