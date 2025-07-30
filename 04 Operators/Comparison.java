0086868a8ae19ff62effabe8fdf50bfa6f895c08
MAHI-VASHISTHA <mahivashistha65@gmail.com>
2025-07-30 12:20:13 +0530

    Add files via upload

---
+++ 04 Operators/Comparison.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class Comparison {
+	
+
+	public static void main(String[] args) {
+
+		int a = 50;
+		float b = 60;
+		
+		System.out.println(a>b);	
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+
+}
+}
