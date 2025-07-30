8b355c429543f57815132a6e89cd10534e2db226
MAHI-VASHISTHA <mahivashistha65@gmail.com>
2025-07-30 13:39:32 +0530

    Add files via upload

---
+++ 15 Composition/Demo.java
@@ -0,0 +1,13 @@
+package non;
+
+public class Demo {
+		int id;
+		String name;
+		String cource;
+
+			public void hello() {
+				System.out.println("this is a method");
+			}
+		}
+
+
---
+++ 15 Composition/Text.java
@@ -0,0 +1,8 @@
+package non;
+
+public class Text {
+public static void main(String[] args) {
+	Demo d=new Demo();
+	d.hello();
+}
+}
