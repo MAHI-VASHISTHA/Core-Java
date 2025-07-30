af436a715bcac852fb8c723636a8d4cd3b80f087
MAHI-VASHISTHA <mahivashistha65@gmail.com>
2025-07-30 12:33:46 +0530

    Add files via upload

---
+++ 05 Conditional Statements/IfStatement2.java
@@ -0,0 +1,27 @@
+package Statements;
+
+public class IfStatement2 {
+	public static void main(String[] args) {
+
+		int a = 1;
+		int b = 4;
+
+		if (a++ == b) {
+			System.out.println(a++);
+		}
+		if (a++ == b) {
+			System.out.println(a++);
+		}
+		if (a++ == b) {
+			System.out.println(a++);
+		}
+		if (a++ == b) {
+			System.out.println(a++);
+		}
+		if (a++ == b) {
+			System.out.println(a++);
+		}
+		System.out.println(a);
+
+	}
+}
