334bf258dbb6d7b8f552d459c21ead43936141b2
MAHI-VASHISTHA <mahivashistha65@gmail.com>
2025-07-30 11:58:38 +0530

    Add files via upload

---
+++ DataTypes/PrimitiveDatatypes.java
@@ -0,0 +1,28 @@
+package Primitive;
+
+public class PrimitiveDatatypes {
+public static void main(String[] args){
+	
+//	byte b=125;    range-->(-128-127)
+//	short s=25486;  (2byte)
+//	int a=1235;         (4byte)
+//	long l=158931478529l;
+	
+//	-----------------------------------------------
+	float b=123.25f;
+	double d=8569.552;
+	
+//	------------------------------------------------
+	
+	char c='5';
+	char ch='g';
+
+//	-------------------------------------------------
+	
+	Boolean bo=true;
+	Boolean bol=false;
+	
+//	-------------------------------------------------
+
+}
+}
