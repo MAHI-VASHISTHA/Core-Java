fc608302bde4703fad81c6a7de4f95f28b0e3a15
MAHI-VASHISTHA <mahivashistha65@gmail.com>
2025-07-30 12:18:56 +0530

    Add files via upload

---
+++ 04 Operators/Arithmatic.java
@@ -0,0 +1,28 @@
+package demo;
+
+public class Arithmatic {
+	public static void main(String[] args) {
+
+		int a = 50;
+		float b = 25.5f;
+		
+		System.out.println(a+b);	
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+
+//		--------------------------------------
+		
+		int p = 0;
+		int q = 25;
+		
+		System.out.println(p+q);	
+		System.out.println(p-q);
+		System.out.println(p*q);
+		System.out.println(p/q);
+		System.out.println(p%q);
+
+
+	}
+}
