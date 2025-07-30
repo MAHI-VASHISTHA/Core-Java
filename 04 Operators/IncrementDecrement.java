a92f5246fb746e958e737fe1d7f508acec1ad0ad
MAHI-VASHISTHA <mahivashistha65@gmail.com>
2025-07-30 12:20:38 +0530

    Add files via upload

---
+++ 04 Operators/IncrementDecrement.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class IncrementDecrement {
+	
+public static void main(String [] args) {
+	
+	int a=20;
+//	int b=a++;
+	
+	int b=--a;
+	
+	System.out.println(b);
+	
+	
+}	
+}
